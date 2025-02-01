175902665f18e0c50c23f01e3b608761765c29ab
hsacog <hsacog@gmail.com>
2025-02-01 11:24:47 +0900

    add jenkins helm settings

---
+++ baremetal/3.jenkins-helm-setup.sh
@@ -0,0 +1,118 @@
+kubectl create namespace jenkins
+helm repo add jenkinsci https://charts.jenkins.io
+helm repo update
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: jenkins-pv
+  namespace: jenkins
+spec:
+  storageClassName: jenkins-pv
+  accessModes:
+  - ReadWriteOnce
+  capacity:
+    storage: 20Gi
+  persistentVolumeReclaimPolicy: Retain
+  hostPath:
+    path: /data/jenkins-volume/
+
+---
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: jenkins-pv
+provisioner: kubernetes.io/no-provisioner
+volumeBindingMode: WaitForFirstConsumer
+EOF
+
+kubectl apply -f - <<EOF
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: jenkins
+  namespace: jenkins
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: jenkins
+rules:
+- apiGroups:
+  - '*'
+  resources:
+  - statefulsets
+  - services
+  - replicationcontrollers
+  - replicasets
+  - podtemplates
+  - podsecuritypolicies
+  - pods
+  - pods/log
+  - pods/exec
+  - podpreset
+  - poddisruptionbudget
+  - persistentvolumes
+  - persistentvolumeclaims
+  - jobs
+  - endpoints
+  - deployments
+  - deployments/scale
+  - daemonsets
+  - cronjobs
+  - configmaps
+  - namespaces
+  - events
+  - secrets
+  verbs:
+  - create
+  - get
+  - watch
+  - delete
+  - list
+  - patch
+  - update
+- apiGroups:
+  - ""
+  resources:
+  - nodes
+  verbs:
+  - get
+  - list
+  - watch
+  - update
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: jenkins
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: jenkins
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: Group
+  name: system:serviceaccounts:jenkins
+EOF
+
+helm install jenkins -n jenkins -f jenkins-values.yaml jenkinsci/jenkins
+
+# setting jenkins config
+# 1. docker-hub token
+kubectl create secret docker-registry regcred -n jenkins \
+--docker-username=$DOCKER_USERNAME \
+--docker-password=$DOCKER_PASSWORD \
+--docker-server=https://index.docker.io/v1/
+
+echo $(echo $(kubectl get secret -n jenkins jenkins -o jsonpath="{.data.jenkins-admin-password}") | base64 --decode)
--- baremetal/README.md
+++ baremetal/README.md
@@ -1,1 +1,25 @@
 config for bare metal server
+
+## TODOs
+- 3.jenkins-helm-setup 에서 jenkins-pv를 사용하도록 설
+    - 현재 KinD 설정이 worker node 3개여서, hostPath를 이용하기 위해서는 추가로 nodeSelector 설정이 필요해 보임.
+
+## 3.jenkins-helm-setup.sh
+### script 실행 전
+- `export DOCKER_USERNAME=<docker hub id>`, `export DOCKER_PASSWORD=<docker hub personal access token`
+
+### script 실행 후
+- JenkinsLocation의 URL을 실제 접속한 URL로 변경해주어야 한다.
+- pipeline script: `git branch: 'main', credentialsId: 'github-token', url: 'https://github.com/hsacog/blog'`
+    - Jenkins 설정의 Credentials 에서, Kind는 `Username with password`, Username은 `github id`, Password는 `github PAT`, ID는 `github-token` 으로 설정해서 생성한다.
+
+## Jenkins
+### JDK 21 설치
+Amazon corretto를 사용하면 된다. (`https://docs.aws.amazon.com/corretto/latest/corretto-21-ug/downloads-list.html`)
+
+Dashboard > Jenkins 관리 > Tools
+JDK installations > Add JDK
+Install automatically 에 체크
+Download URL for binary archive: `https://corretto.aws/downloads/latest/amazon-corretto-21-aarch64-linux-jdk.tar.gz`
+Subdirectory of extracted archive: `amazon-corretto-21.0.6.7.1-linux-aarch64`
+
